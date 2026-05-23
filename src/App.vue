ebbb8a60ae75f67eb870867522f097ce740e7a2c
alexvarivo <alexandra.varivoda@gmail.com>
2026-05-23 03:32:19 -0700

    Set up basic app layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <div class="app">
+    <h1>Media Tracker</h1>
+    <p>Track your favorite books, shows/movies, games, and more.</p>
+  </div>
 </template>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  background-color: #f4f4f4;
+}
+
+.app {
+  padding: 2rem;
+}
+
+h1 {
+  margin-bottom: 0.5rem;
+}
+
+</style>
